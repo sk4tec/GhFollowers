9007f933d69b9d30030877e65906b188817a6462
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-10-29 19:17:02 +0100

    add:

---
+++ GhFollowers/FollowerListVC.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerListVC.swift
+//  GhFollowers
+//
+//  Created by Sunjay Kalsi on 25/10/2021.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var userName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+
+    }
+
+}
+
