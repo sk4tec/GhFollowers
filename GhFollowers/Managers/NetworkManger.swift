34805f66bd8df17888945785cc90790177b2d432
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-11-16 17:05:46 +0000

    changdes

---
+++ GhFollowers/Managers/NetworkManger.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManger.swift
+//  GhFollowers
+//
+//  Created by Sunjay Kalsi on 08/11/2021.
+//
+
+import Foundation
+
+class NetWorkManager {
+    let shared = NetWorkManager()
+    
+    private init() {}
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String)) {
+        
+    }
+}
