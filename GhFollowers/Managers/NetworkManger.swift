c38daf985731d0f81566d212d96069bbae56c6d3
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-11-22 19:11:07 +0000

    add: netowrk manager

--- GhFollowers/Managers/NetworkManger.swift
+++ GhFollowers/Managers/NetworkManger.swift
@@ -9,10 +9,35 @@ import Foundation
 
 class NetWorkManager {
     let shared = NetWorkManager()
+    let baseUrl = "https://api.github.com/user/"
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String)) {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+        let endpoint = baseUrl + "/users/\(username)/followers?per_page=100&page=\(page)"
         
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request. Please try again")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server. Please try again")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data recieved from the server was invalid. Please try again.")
+                return
+            }
+        }
+        
+        task.resume()
     }
 }
