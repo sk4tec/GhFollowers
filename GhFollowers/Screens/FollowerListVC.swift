570d49c82a50ddb5dc103b74180b9e37918ce758
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-12-01 19:16:37 +0000

    add: three column flow layout

--- GhFollowers/Screens/FollowerListVC.swift
+++ GhFollowers/Screens/FollowerListVC.swift
@@ -25,7 +25,7 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowersCell.self, forCellWithReuseIdentifier: FollowersCell.resuseID)
@@ -34,6 +34,20 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40) // Square + a bit more for the label at the bottom
+        
+        return flowLayout
     }
     
     func getFollowers() {
