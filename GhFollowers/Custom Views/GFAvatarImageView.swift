b977dc98175fcac36ea275b076fa4492af11fd74
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-12-11 12:29:57 +0000

    add: cache the image

--- GhFollowers/Custom Views/GFAvatarImageView.swift
+++ GhFollowers/Custom Views/GFAvatarImageView.swift
@@ -11,6 +11,8 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     let placeholderImage = UIImage(named: "avatar-placeholder")!
+    let cache = NetworkManager.shared.cache
+    private var debugCount = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,7 +31,17 @@ class GFAvatarImageView: UIImageView {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    // we don't need to handle an error at all, we return and by the place holder image is set
     func downloadImage(from urlString: String) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            print("\(debugCount) cache\r")
+            self.image = image
+            debugCount = debugCount + 1
+            return
+        }
+        
         guard let url = URL(string: urlString) else { return }
         
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
@@ -40,9 +52,14 @@ class GFAvatarImageView: UIImageView {
                   let data = data,
                   let self = self,
                   response.statusCode == 200 else { return }
+
+            guard let image = UIImage(data: data) else { return }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            self.debugCount = self.debugCount + 1
+            print("\(self.debugCount) network call\r")
             
             DispatchQueue.main.async {
-                let image = UIImage(data: data)
                 self.image = image
             }
         }
