ebee7b8ec79132eee4750833b56d52f6ac6c1457
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2022-01-23 09:13:39 +0000

    add: GFUserHeader

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,69 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Sunjay Kalsi on 18/01/2022.
+//  Copyright © 2022 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let userNameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView = UIImageView()
+    let locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        layoutUI()
+    }
+    
+    func addSubViews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(userNameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+    func layoutUI() {
+        let padding: CGFloat = 20
+        let textImagePadding: CGFloat = 12
+        
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            userNameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 8), //tweak constant
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding)
+            
+        ])
+        
+    }
+}
